d37b4084d4ad75c9f298e406fca101debbe3ca37
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-25 14:08:01 +0200

    MyFooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import MyFooter from "./components/MyFooter";
 import MyNav from "./components/MyNav";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
+      <MyFooter />
     </div>
   );
 }
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,16 @@
+const MyFooter = () => {
+  return (
+    <footer class="footer">
+      <div>
+        <a href="#"></a>
+        <span>&copy; 2024 EpicBook Labs.</span>
+      </div>
+      <div>
+        <span>Powered by </span>
+        <a href="#">Mirko</a>
+      </div>
+    </footer>
+  );
+};
+
+export default MyFooter;
