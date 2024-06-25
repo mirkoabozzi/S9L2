c95d6cb18d9ba6c827cddaa33fc5e37cf89595a5
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-25 14:19:35 +0200

    Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import MyFooter from "./components/MyFooter";
 import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
+      <Welcome />
       <MyFooter />
     </div>
   );
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import { Alert } from "react-bootstrap";
+
+const Welcome = () => {
+  return (
+    <>
+      <Alert> Libri come se piovesse! 😂 </Alert>
+      <p>Tutti i libri che cerchi nel tuo shop preferito!</p>
+    </>
+  );
+};
+export default Welcome;
